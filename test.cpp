22b289640511a57240616cfbfa4d2ed31f58df74
TheNumbat <mslater@nevada.unr.edu>
2023-11-12 15:25:07 -0500

    test

--- test.cpp
+++ test.cpp
@@ -21,16 +21,14 @@
 
 using namespace rpp;
 
-auto lots_of_jobs(Thread::Pool<>& pool, u64 depth) -> Async::Task<> {
+auto lots_of_jobs(Thread::Pool<>& pool, u64 depth) -> Async::Task<u64> {
     if(depth == 0) {
-        co_return;
+        co_return 1;
     }
     co_await pool.suspend();
     auto job0 = lots_of_jobs(pool, depth - 1);
     auto job1 = lots_of_jobs(pool, depth - 1);
-    co_await job0;
-    co_await job1;
-    co_return;
+    co_return co_await job0 + co_await job1;
 };
 
 i32 main() {
@@ -44,8 +42,7 @@ i32 main() {
         Thread::Pool pool;
 
         for(u64 i = 0; i < 1000; i++) {
-            info("lol");
-            lots_of_jobs(pool, 10).block();
+            info("%", lots_of_jobs(pool, 10).block());
         }
     }
 
