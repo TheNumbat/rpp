61520e5d6f627961ad862e28774ed88b0ca1e349
TheNumbat <mslater@nevada.unr.edu>
2023-12-24 09:57:11 -0500

    f32x8

--- rpp/impl/simd.cpp
+++ rpp/impl/simd.cpp
@@ -1,7 +1,7 @@
 
 #include "../simd.h"
 
-#include <smmintrin.h>
+#include <immintrin.h>
 
 namespace rpp::SIMD {
 
@@ -14,6 +14,30 @@ static __m128 of(F32x4 a) {
 
 static F32x4 to(__m128 a) {
     return *reinterpret_cast<F32x4*>(&a);
+}
+
+static __m256 of(F32x8 a) {
+    return *reinterpret_cast<__m256*>(a.data);
+}
+
+static F32x8 to(__m256 a) {
+    return *reinterpret_cast<F32x8*>(&a);
+}
+
+F32x4 F32x4::set1(f32 v) {
+    return to(_mm_set1_ps(v));
+}
+
+F32x4 F32x4::set(f32 x, f32 y, f32 z, f32 w) {
+    return to(_mm_set_ps(x, y, z, w));
+}
+
+F32x4 F32x4::zero() {
+    return to(_mm_setzero_ps());
+}
+
+F32x4 F32x4::one() {
+    return to(_mm_setzero_ps());
 }
 
 i32 F32x4::movemask(F32x4 a) {
@@ -62,6 +86,70 @@ f32 F32x4::dp(F32x4 a, F32x4 b) {
 
 F32x4 F32x4::cmpeq(F32x4 a, F32x4 b) {
     return to(_mm_cmpeq_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::set1(f32 v) {
+    return to(_mm256_set1_ps(v));
+}
+
+F32x8 F32x8::set(f32 a, f32 b, f32 c, f32 d, f32 e, f32 f, f32 g, f32 h) {
+    return to(_mm256_set_ps(a, b, c, d, e, f, g, h));
+}
+
+F32x8 F32x8::zero() {
+    return to(_mm256_setzero_ps());
+}
+
+F32x8 F32x8::one() {
+    return to(_mm256_setzero_ps());
+}
+
+i32 F32x8::movemask(F32x8 a) {
+    return _mm256_movemask_ps(of(a));
+}
+
+F32x8 F32x8::add(F32x8 a, F32x8 b) {
+    return to(_mm256_add_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::sub(F32x8 a, F32x8 b) {
+    return to(_mm256_sub_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::mul(F32x8 a, F32x8 b) {
+    return to(_mm256_mul_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::div(F32x8 a, F32x8 b) {
+    return to(_mm256_div_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::min(F32x8 a, F32x8 b) {
+    return to(_mm256_min_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::max(F32x8 a, F32x8 b) {
+    return to(_mm256_max_ps(of(a), of(b)));
+}
+
+F32x8 F32x8::floor(F32x8 a) {
+    return to(_mm256_floor_ps(of(a)));
+}
+
+F32x8 F32x8::ceil(F32x8 a) {
+    return to(_mm256_ceil_ps(of(a)));
+}
+
+F32x8 F32x8::abs(F32x8 a) {
+    return to(_mm256_andnot_ps(_mm256_set1_ps(-0.0f), of(a)));
+}
+
+f32 F32x8::dp(F32x8 a, F32x8 b) {
+    return _mm256_cvtss_f32(_mm256_dp_ps(of(a), of(b), 0xff));
+}
+
+F32x8 F32x8::cmpeq(F32x8 a, F32x8 b) {
+    return to(_mm256_cmp_ps(of(a), of(b), 0));
 }
 
 } // namespace rpp::SIMD
--- rpp/simd.h
+++ rpp/simd.h
@@ -8,19 +8,10 @@ namespace rpp::SIMD {
 struct F32x4 {
     alignas(16) f32 data[4];
 
-    static constexpr F32x4 set1(f32 v) {
-        return {v, v, v, v};
-    }
-    static constexpr F32x4 set(f32 x, f32 y, f32 z, f32 w) {
-        return {x, y, z, w};
-    }
-    static constexpr F32x4 zero() {
-        return {0.0f, 0.0f, 0.0f, 0.0f};
-    }
-    static constexpr F32x4 one() {
-        return {1.0f, 1.0f, 1.0f, 1.0f};
-    }
-
+    static F32x4 set1(f32 v);
+    static F32x4 set(f32 x, f32 y, f32 z, f32 w);
+    static F32x4 zero();
+    static F32x4 one();
     static F32x4 add(F32x4 a, F32x4 b);
     static F32x4 sub(F32x4 a, F32x4 b);
     static F32x4 mul(F32x4 a, F32x4 b);
@@ -33,6 +24,27 @@ struct F32x4 {
     static f32 dp(F32x4 a, F32x4 b);
     static F32x4 cmpeq(F32x4 a, F32x4 b);
     static i32 movemask(F32x4 a);
+};
+
+struct F32x8 {
+    alignas(32) f32 data[4];
+
+    static F32x8 set1(f32 v);
+    static F32x8 set(f32 a, f32 b, f32 c, f32 d, f32 e, f32 f, f32 g, f32 h);
+    static F32x8 zero();
+    static F32x8 one();
+    static F32x8 add(F32x8 a, F32x8 b);
+    static F32x8 sub(F32x8 a, F32x8 b);
+    static F32x8 mul(F32x8 a, F32x8 b);
+    static F32x8 div(F32x8 a, F32x8 b);
+    static F32x8 min(F32x8 a, F32x8 b);
+    static F32x8 max(F32x8 a, F32x8 b);
+    static F32x8 floor(F32x8 a);
+    static F32x8 ceil(F32x8 a);
+    static F32x8 abs(F32x8 a);
+    static f32 dp(F32x8 a, F32x8 b);
+    static F32x8 cmpeq(F32x8 a, F32x8 b);
+    static i32 movemask(F32x8 a);
 };
 
 } // namespace rpp::SIMD
