3d64e4166295430d670332c234a1e9deac67b693
GustavoSilvera <grs5382@gmail.com>
2024-01-20 23:36:27 -0800

    Use templates for F32x4 and F32x8

--- rpp/impl/simd.cpp
+++ rpp/impl/simd.cpp
@@ -8,8 +8,8 @@ static_assert(alignof(F32x4) == 16);
 static_assert(sizeof(F32x8) == 32);
 static_assert(alignof(F32x8) == 32);
 
-typedef float float4 __attribute__((ext_vector_type(4)));
-typedef float float8 __attribute__((ext_vector_type(8)));
+typedef f32 float4 __attribute__((ext_vector_type(4)));
+typedef f32 float8 __attribute__((ext_vector_type(8)));
 
 [[nodiscard]] static float4 of(F32x4 a) noexcept {
     return {a.data[0], a.data[1], a.data[2], a.data[3]};
@@ -27,156 +27,110 @@ typedef float float8 __attribute__((ext_vector_type(8)));
     return {a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]};
 }
 
-[[nodiscard]] F32x4 F32x4::set1(f32 v) noexcept {
-    return to(float4(v));
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::set1(f32 v) noexcept {
+    return to(floatT(v));
 }
 
+template<>
 [[nodiscard]] F32x4 F32x4::set(f32 x, f32 y, f32 z, f32 w) noexcept {
     return to((float4){x, y, z, w});
 }
 
-[[nodiscard]] F32x4 F32x4::zero() noexcept {
-    return set1(0);
-}
-
-[[nodiscard]] F32x4 F32x4::one() noexcept {
-    return set1(1);
-}
-
-[[nodiscard]] i32 F32x4::movemask(F32x4 a) noexcept {
-    /// TODO: do we just take the sign bit as the msb for floats?
-    i32 mask0 = ((a.data[0] > 0) >> 31);
-    i32 mask1 = ((a.data[1] > 0) >> 31);
-    i32 mask2 = ((a.data[2] > 0) >> 31);
-    i32 mask3 = ((a.data[3] > 0) >> 31);
-    return (mask3 << 3) | (mask2 << 2) | (mask1 << 1) | (mask0 << 0);
-}
-
-[[nodiscard]] F32x4 F32x4::add(F32x4 a, F32x4 b) noexcept {
-    return to(of(a) + of(b));
-}
-
-[[nodiscard]] F32x4 F32x4::sub(F32x4 a, F32x4 b) noexcept {
-    return to(of(a) - of(b));
-}
-
-[[nodiscard]] F32x4 F32x4::mul(F32x4 a, F32x4 b) noexcept {
-    return to(of(a) - of(b));
-}
-
-[[nodiscard]] F32x4 F32x4::div(F32x4 a, F32x4 b) noexcept {
-    return to(of(a) / of(b));
-}
-
-[[nodiscard]] F32x4 F32x4::min(F32x4 a, F32x4 b) noexcept {
-    return to(__builtin_elementwise_min(of(a), of(b)));
-}
-
-[[nodiscard]] F32x4 F32x4::max(F32x4 a, F32x4 b) noexcept {
-    return to(__builtin_elementwise_max(of(a), of(b)));
-}
-
-[[nodiscard]] F32x4 F32x4::floor(F32x4 a) noexcept {
-    return to(__builtin_elementwise_floor(of(a)));
-}
-
-[[nodiscard]] F32x4 F32x4::ceil(F32x4 a) noexcept {
-    return to(__builtin_elementwise_ceil(of(a)));
-}
-
-[[nodiscard]] F32x4 F32x4::abs(F32x4 a) noexcept {
-    return to(__builtin_elementwise_abs(of(a)));
-}
-
-[[nodiscard]] f32 F32x4::dp(F32x4 a, F32x4 b) noexcept {
-    F32x4 m = mul(a, b);
-    return m.data[0] + m.data[1] + m.data[2] + m.data[3];
-}
-
-[[nodiscard]] F32x4 F32x4::cmpeq(F32x4 a, F32x4 b) noexcept {
-    /// TODO: slightly different from
-    /// https://doc.rust-lang.org/beta/core/arch/x86_64/fn._mm_cmpeq_ps.html
-    auto i32x4 = (of(a) == of(b));
-    return {static_cast<f32>(i32x4[0]), static_cast<f32>(i32x4[1]), static_cast<f32>(i32x4[2]),
-            static_cast<f32>(i32x4[3])};
-}
-
-[[nodiscard]] F32x8 F32x8::set1(f32 v) noexcept {
-    return to(float8(v));
-}
-
+template<>
 [[nodiscard]] F32x8 F32x8::set(f32 a, f32 b, f32 c, f32 d, f32 e, f32 f, f32 g, f32 h) noexcept {
     return to((float8){a, b, c, d, e, f, g, h});
 }
 
-[[nodiscard]] F32x8 F32x8::zero() noexcept {
-    return set1(0);
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::zero() noexcept {
+    return to(floatT(0));
 }
 
-[[nodiscard]] F32x8 F32x8::one() noexcept {
-    return set1(1);
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::one() noexcept {
+    return to(floatT(1));
 }
 
-[[nodiscard]] i32 F32x8::movemask(F32x8 a) noexcept {
-    /// TODO: do we just take the sign bit as the msb for floats?
-    i32 mask0 = ((a.data[0] > 0) >> 31);
-    i32 mask1 = ((a.data[1] > 0) >> 31);
-    i32 mask2 = ((a.data[2] > 0) >> 31);
-    i32 mask3 = ((a.data[3] > 0) >> 31);
-    i32 mask4 = ((a.data[4] > 0) >> 31);
-    i32 mask5 = ((a.data[5] > 0) >> 31);
-    i32 mask6 = ((a.data[6] > 0) >> 31);
-    i32 mask7 = ((a.data[7] > 0) >> 31);
-    return (mask7 << 7) | (mask6 << 6) | (mask5 << 5) | (mask4 << 4) | (mask3 << 3) | (mask2 << 2) |
-           (mask1 << 1) | (mask0 << 0);
+template<i32 T>
+[[nodiscard]] i32 F32x<T>::movemask(F32x<T> a) noexcept {
+    // Set each bit of mask dst based on the most significant bit of the corresponding packed
+    // single-precision (32-bit) floating-point element in a.
+    // https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm_movemask_ps
+    i32 ret = 0;
+    for(i32 i = 0; i < T; i++) {
+        ret |= (a.data[i] > 0) << i;
+    }
+    return ret;
 }
 
-[[nodiscard]] F32x8 F32x8::add(F32x8 a, F32x8 b) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::add(F32x<T> a, F32x<T> b) noexcept {
     return to(of(a) + of(b));
 }
 
-[[nodiscard]] F32x8 F32x8::sub(F32x8 a, F32x8 b) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::sub(F32x<T> a, F32x<T> b) noexcept {
     return to(of(a) - of(b));
 }
 
-[[nodiscard]] F32x8 F32x8::mul(F32x8 a, F32x8 b) noexcept {
-    return to(of(a) * of(b));
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::mul(F32x<T> a, F32x<T> b) noexcept {
+    return to(of(a) - of(b));
 }
 
-[[nodiscard]] F32x8 F32x8::div(F32x8 a, F32x8 b) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::div(F32x<T> a, F32x<T> b) noexcept {
     return to(of(a) / of(b));
 }
 
-[[nodiscard]] F32x8 F32x8::min(F32x8 a, F32x8 b) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::min(F32x<T> a, F32x<T> b) noexcept {
     return to(__builtin_elementwise_min(of(a), of(b)));
 }
 
-[[nodiscard]] F32x8 F32x8::max(F32x8 a, F32x8 b) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::max(F32x<T> a, F32x<T> b) noexcept {
     return to(__builtin_elementwise_max(of(a), of(b)));
 }
 
-[[nodiscard]] F32x8 F32x8::floor(F32x8 a) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::floor(F32x<T> a) noexcept {
     return to(__builtin_elementwise_floor(of(a)));
 }
 
-[[nodiscard]] F32x8 F32x8::ceil(F32x8 a) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::ceil(F32x<T> a) noexcept {
     return to(__builtin_elementwise_ceil(of(a)));
 }
 
-[[nodiscard]] F32x8 F32x8::abs(F32x8 a) noexcept {
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::abs(F32x<T> a) noexcept {
     return to(__builtin_elementwise_abs(of(a)));
 }
 
-[[nodiscard]] f32 F32x8::dp(F32x8 a, F32x8 b) noexcept {
-    F32x8 m = mul(a, b);
-    return m.data[0] + m.data[1] + m.data[2] + m.data[3] + m.data[4] + m.data[5] + m.data[6] +
-           m.data[7];
+template<i32 T>
+[[nodiscard]] f32 F32x<T>::dp(F32x<T> a, F32x<T> b) noexcept {
+    F32x4 m = mul(a, b);
+    // No built-in to reduce float vectors, lets just do it manually
+    f32 ret = 0.f;
+    for(i32 i = 0; i < T; i++) {
+        ret += m.data[i];
+    }
+    return ret;
 }
 
-[[nodiscard]] F32x8 F32x8::cmpeq(F32x8 a, F32x8 b) noexcept {
-    auto i32x8 = (of(a) == of(b));
-    return {static_cast<f32>(i32x8[0]), static_cast<f32>(i32x8[1]), static_cast<f32>(i32x8[2]),
-            static_cast<f32>(i32x8[3]), static_cast<f32>(i32x8[4]), static_cast<f32>(i32x8[5]),
-            static_cast<f32>(i32x8[6]), static_cast<f32>(i32x8[7])};
+template<i32 T>
+[[nodiscard]] F32x<T> F32x<T>::cmpeq(F32x<T> a, F32x<T> b) noexcept {
+    /// NOTE: Implemented following
+    /// https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm_cmpeq_ps
+    const auto cmp_res = (of(a) == of(b));
+    F32x<T> ret;
+    for(i32 i = 0; i < T; i++) {
+        i32 result = cmp_res[i] ? 0xFFFFFFFF : 0;
+        // directly assign the bits of the float
+        memcpy(&ret.data[0], &result, sizeof(result));
+    }
+    return ret;
 }
 } // namespace rpp::SIMD
--- rpp/simd.h
+++ rpp/simd.h
@@ -7,46 +7,32 @@
 
 namespace rpp::SIMD {
 
-struct F32x4 {
-    alignas(16) f32 data[4];
+template<i32 T>
+struct F32x {
+    alignas(4 * T) f32 data[T];
+    typedef f32 floatT __attribute__((ext_vector_type(T)));
 
-    [[nodiscard]] static F32x4 set1(f32 v) noexcept;
-    [[nodiscard]] static F32x4 set(f32 x, f32 y, f32 z, f32 w) noexcept;
-    [[nodiscard]] static F32x4 zero() noexcept;
-    [[nodiscard]] static F32x4 one() noexcept;
-    [[nodiscard]] static F32x4 add(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 sub(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 mul(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 div(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 min(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 max(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 floor(F32x4 a) noexcept;
-    [[nodiscard]] static F32x4 ceil(F32x4 a) noexcept;
-    [[nodiscard]] static F32x4 abs(F32x4 a) noexcept;
-    [[nodiscard]] static f32 dp(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static F32x4 cmpeq(F32x4 a, F32x4 b) noexcept;
-    [[nodiscard]] static i32 movemask(F32x4 a) noexcept;
+    [[nodiscard]] static F32x<T> set1(f32 v) noexcept;
+    [[nodiscard]] static F32x<T> set(f32 x, f32 y, f32 z, f32 w) noexcept;
+    [[nodiscard]] static F32x<T> set(f32 a, f32 b, f32 c, f32 d, f32 e, f32 f, f32 g,
+                                     f32 h) noexcept;
+    [[nodiscard]] static F32x<T> zero() noexcept;
+    [[nodiscard]] static F32x<T> one() noexcept;
+    [[nodiscard]] static F32x<T> add(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> sub(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> mul(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> div(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> min(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> max(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> floor(F32x<T> a) noexcept;
+    [[nodiscard]] static F32x<T> ceil(F32x<T> a) noexcept;
+    [[nodiscard]] static F32x<T> abs(F32x<T> a) noexcept;
+    [[nodiscard]] static f32 dp(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static F32x<T> cmpeq(F32x<T> a, F32x<T> b) noexcept;
+    [[nodiscard]] static i32 movemask(F32x<T> a) noexcept;
 };
 
-struct F32x8 {
-    alignas(32) f32 data[8];
-
-    [[nodiscard]] static F32x8 set1(f32 v) noexcept;
-    [[nodiscard]] static F32x8 set(f32 a, f32 b, f32 c, f32 d, f32 e, f32 f, f32 g, f32 h) noexcept;
-    [[nodiscard]] static F32x8 zero() noexcept;
-    [[nodiscard]] static F32x8 one() noexcept;
-    [[nodiscard]] static F32x8 add(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 sub(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 mul(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 div(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 min(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 max(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 floor(F32x8 a) noexcept;
-    [[nodiscard]] static F32x8 ceil(F32x8 a) noexcept;
-    [[nodiscard]] static F32x8 abs(F32x8 a) noexcept;
-    [[nodiscard]] static f32 dp(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static F32x8 cmpeq(F32x8 a, F32x8 b) noexcept;
-    [[nodiscard]] static i32 movemask(F32x8 a) noexcept;
-};
+typedef F32x<4> F32x4;
+typedef F32x<8> F32x8;
 
 } // namespace rpp::SIMD
